af45e0c9e129b19850143bc1eeb796303539a529
ShristiMohapatra <84072850+ShristiMohapatra@users.noreply.github.com>
2021-08-22 21:53:45 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,31 +1,29 @@
- name_of_the_student_array=[];
+menu_list_array = ["Veg Margherita Pizza",//add more items
+                    ];
 
- function submit(){
+function getmenu(){
+var htmldata;
+menu_list_array.sort();
+        for(var i=0;i<menu_list_array.length;i++){
+            htmldata=htmldata+ menu_list_array[i] + '<br>'
+        }
+        document.getElementById("    ").innerHTML = htmldata;
+        //give the appropriate id name as display_menu  
+}
 
-    var name_1= document.getElementById("name_of_the_student_1").value;
-    var name_2= document.getElementById("name_of_the_student_2").value;
-    var name_3= document.getElementById("name_of_the_student_3").value;
-    var name_4= document.getElementById("name_of_the_student_4").value;
-    
-    name_of_the_student_array.push(name_1);
-    name_of_the_student_array.push(name_2);
-    name_of_the_student_array.push(name_3);
-    name_of_the_student_array.push(name_4);
+function add_item(){
+var htmldata;
+var item=document.getElementById("add_item").value;
+        // use the sort function as - menu_list_array.sort();
+        htmldata=""
+        for(var i=0;i<menu_list_array.length;i++){
+            htmldata=htmldata+imgtags+ menu_list_array[i]+'<br>';
+        }
+         document.getElementById("display_addedmenu").innerHTML = htmldata;		
+      }
 
-    console.log(name_of_the_student_array);
-
-    document.getElementById("display_name").innerHTML=name_of_the_student_array;
-
-    document.getElementById("submit_button").style.display="none";
-    document.getElementById("sort_button").style.display="inline-block";
- }
-
- function sort(){
-
-    name_of_the_student_array.sort();
-     
-    console.log(name_of_the_student_array);
-
-    document.getElementById("display_name").innerHTML=name_of_the_student_array;
-   
- }
+function add_top(){
+    var item=document.getElementById("add_item").value;
+    menu_list_array.push(item);
+	add_item();
+}
